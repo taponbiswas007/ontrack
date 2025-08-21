d94865cc18573f6fbbf7fff46c5bbf0a52d45a98
Tapon chandra sarker <108454446+taponbiswas007@users.noreply.github.com>
2025-08-22 01:38:25 +0600

    dsfsdf

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,25 @@ $(document).ready(function() {
     });
 });
 
+// back to top button start
+ $(document).ready(function () {
+        // Show button after scrolling down 200px
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > 200) {
+                $('#backToTop').fadeIn();
+            } else {
+                $('#backToTop').fadeOut();
+            }
+        });
+
+        // Scroll to top smoothly
+        $('#backToTop').click(function () {
+            $('html, body').animate({ scrollTop: 0 }, 600);
+            return false;
+        });
+    });
+// back to top button end
+
 
 
  
